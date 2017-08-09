b89e924b6c2925762b835896fb5840fcb3a598c1
cameron581 <rickle581@gmail.com>
2017-08-09 05:57:34 -0500

    ocn: Add and enable VoLTE (1/2) Squashed @kumajaya Change-Id: Ibd5508e7a67a4c45a2443f0ec8d4bd52e7e7e255 @deepakkundra Change-Id: I9a1f6c8b2cb6b89ad1e93236cefbeb4fd2b52f8a @Ravi Paluri Change-Id: I1bee1d68f7c9218bbfdf0ab2117a28f16ddad3cb @Suresh Koleti Change-Id: I603209cc4cb891c1ef9103958d81ae7d65f0043c @HridayHS Change-Id: Iff59a04c8cb5de19d7aec4943f68b447ea614f4d

--- device.mk
+++ device.mk
@@ -167,15 +167,38 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     liblog_htc
 
+# IMS properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+# Widevine DRM symbol, boringssl-compat subset
+PRODUCT_PACKAGES += \
+    libshim_boringssl
+
 # Init
 PRODUCT_PACKAGES += \
+    init.qti.ims.sh \
     fstab.qcom \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     ueventd.qcom.rc \
     init.qcom.syspart_fixup.sh \
-    init.qcom.sh
+    init.qcom.sh\
+    init.target.rc
 
 # Input
 PRODUCT_COPY_FILES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,6 +4,22 @@ include $(CLEAR_VARS)
 # Device init scripts
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qti.ims.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qti.ims.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
@@ -58,4 +74,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.syspart_fixup.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
